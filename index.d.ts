e095fd55002ffdb7167d595af1f9ed5101f84c21
楼教主 <cik520@qq.com>
2019-03-13 09:27:11 +0800

    优化声明

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,17 @@
 interface IPInfo {
   /** IP地址 */
   ip: string;
-  /** 省市/局域网/网络类型 */
+  /** 地址信息: 省市/局域网/网络类型 */
   country: string;
-  /** 运营商/公司/组织/描述信息 */
+  /** 描述信息: 运营商/公司/组织/描述信息 */
   area: string;
 }
 export declare class QQwry {
-  buffer: Buffer;
-  ipBegin: number;
-  ipEnd: number;
   /**
    * qqwry.dat 地址
    * @param path 路径
    */
-  constructor(path: string);
+  constructor(path?: string);
   /**
    * 搜索IP信息
    * @param ip IP
